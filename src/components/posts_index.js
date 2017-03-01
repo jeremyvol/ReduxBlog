3a760ad47db59e44498ca4c5d9c276dcf9245b19
Jérémy  VOLLANT <jeremy.vollant@altran.com>
2017-03-01 18:15:25 +0100

    new post form

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsIndex extends Component {
-  componentWillMount() {
-    this.props.fetchPosts();
-  }
+    componentWillMount() {
+        this.props.fetchPosts();
+    }
 
-  render() {
-    return (
-      <div>List of blog posts</div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div className="text-xs-right">
+                    <Link to="/posts/new" className="btn btn-primary">
+                        Add a post
+                    </Link>
+                </div>
+                List of blog posts
+            </div>
+        );
+    }
 }
 
 // function mapDispatchToProps(dispatch) {
