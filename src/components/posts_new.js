34dee20efd8f8afcc6455301b42be366e1d587c8
JeremyV <j.vollant@gmail.com>
2017-03-02 00:33:08 +0100

    post created

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -8,12 +8,22 @@ class PostsNew extends Component {
         router: PropTypes.object
     };
 
+    onSubmit(props) {
+      this.props.createPost(props)
+      .then(() => {
+        // blog post has been created, navigate the user to the index
+        // We navigate by calling this.context.router.push
+        // with the new path to navbigate to
+        this.context.router.push('/');
+      });
+    }
+
     render() {
         const { fields: {title, categories, content }, handleSubmit } = this.props;
         //=== const handleSubmit = this.props.handleSubmit;
         //=== const title = this.props.fields.title
         return (
-            <form onSubmit={handleSubmit(this.props.createPost)}>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <h3>Create A New Post</h3>
 
                 <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}` }>
