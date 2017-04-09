4aa3d5ffd863fd9cdeee89b7df951081b471b1b5
JeremyV <j.vollant@gmail.com>
2017-04-10 01:04:08 +0200

    Done

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchPosts } from '../actions/index';
-import { Link } from 'react-router';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions/index';
+import {Link} from 'react-router';
 
 class PostsIndex extends Component {
     componentWillMount() {
@@ -16,15 +16,35 @@ class PostsIndex extends Component {
                         Add a post
                     </Link>
                 </div>
-                List of blog posts
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
+
+    renderPosts() {
+        return  this.props.posts.map((post) => {
+            return (
+                <li className="list-group-item" key={post.id}>
+                    <Link to={"posts/" + post.id}>
+                        <span className="pull-xs-right">{post.categories}</span>
+                        <strong>{post.title}</strong>
+                    </Link>
+                </li>
+            );
+        });
+    }
+}
+
+function mapStateToProps(state) {
+    return {posts: state.posts.all};
 }
 
 // function mapDispatchToProps(dispatch) {
 //   return bindActionCreators({ fetchPosts }, dispatch);
 // }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, {fetchPosts})(PostsIndex);
 // null instead of mapStateToProps
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,7 +1,7 @@
-import React, { Component, PropTypes } from 'react';
-import { reduxForm } from 'redux-form';
-import { createPost } from '../actions/index';
-import { Link } from 'react-router';
+import React, {Component, PropTypes} from 'react';
+import {reduxForm} from 'redux-form';
+import {createPost} from '../actions/index';
+import {Link} from 'react-router';
 
 class PostsNew extends Component {
     static contextTypes = {
@@ -9,44 +9,62 @@ class PostsNew extends Component {
     };
 
     onSubmit(props) {
-      this.props.createPost(props)
-      .then(() => {
-        // blog post has been created, navigate the user to the index
-        // We navigate by calling this.context.router.push
-        // with the new path to navbigate to
-        this.context.router.push('/');
-      });
+        this.props.createPost(props).then(() => {
+            // blog post has been created, navigate the user to the index
+            // We navigate by calling this.context.router.push
+            // with the new path to navbigate to
+            this.context.router.push('/');
+        });
     }
 
     render() {
-        const { fields: {title, categories, content }, handleSubmit } = this.props;
+        const {
+            fields: {
+                title,
+                categories,
+                content
+            },
+            handleSubmit
+        } = this.props;
         //=== const handleSubmit = this.props.handleSubmit;
         //=== const title = this.props.fields.title
         return (
             <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <h3>Create A New Post</h3>
 
-                <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}` }>
+                <div className={`form-group ${title.touched && title.invalid
+                    ? 'has-danger'
+                    : ''}`}>
                     <label>Title</label>
-                    <input type="text" className="form-control" {...title} />
+                    <input type="text" className="form-control" {...title}/>
                     <div className="text-help">
-                        {title.touched ? title.error : ''}
+                        {title.touched
+                            ? title.error
+                            : ''}
                     </div>
                 </div>
 
-                <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
+                <div className={`form-group ${categories.touched && categories.invalid
+                    ? 'has-danger'
+                    : ''}`}>
                     <label>Categories</label>
-                    <input type="text" className="form-control" {...categories} />
+                    <input type="text" className="form-control" {...categories}/>
                     <div className="text-help">
-                        {categories.touched ? categories.error : ''}
+                        {categories.touched
+                            ? categories.error
+                            : ''}
                     </div>
                 </div>
 
-                <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
+                <div className={`form-group ${content.touched && content.invalid
+                    ? 'has-danger'
+                    : ''}`}>
                     <label>Content</label>
-                    <textarea className="form-control" {...content} />
+                    <textarea className="form-control" {...content}/>
                     <div className="text-help">
-                        {content.touched ? content.error : ''}
+                        {content.touched
+                            ? content.error
+                            : ''}
                     </div>
                 </div>
 
@@ -58,7 +76,7 @@ class PostsNew extends Component {
 }
 
 function validate(values) {
-    const errors={};
+    const errors = {};
     if (!values.title) {
         errors.title = 'Enter a username';
     }
@@ -75,6 +93,8 @@ function validate(values) {
 // reduxForm: 1st is form copnfig, 2nd is mapStatetoProps, 3rd is mapDispatchToProps
 export default reduxForm({
     form: 'PostNewForm',
-    fields: ['title','categories','content'],
+    fields: [
+        'title', 'categories', 'content'
+    ],
     validate
-}, null, { createPost })(PostsNew);
+}, null, {createPost})(PostsNew);
